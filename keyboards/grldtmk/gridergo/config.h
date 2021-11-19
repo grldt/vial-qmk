39db7e75460e7dfc688b0860285b700336bae5fb
grldt <geraldstephanus@gmail.com>
2021-11-19 22:40:21 +0700

    gridergo

--- keyboards/grldtmk/gridergo/config.h
+++ keyboards/grldtmk/gridergo/config.h
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x6227
-#define PRODUCT_ID      0x3232
+#define PRODUCT_ID      0x3126
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    grldtmk
 #define PRODUCT         ErgoGrid
@@ -33,10 +33,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROWS 12
 #define MATRIX_COLS 7
 
+// Vial Keyboard ID - "python3 util/vial_generate_keyboard_uid.py"
+#define VIAL_KEYBOARD_UID {0x90, 0x10, 0x9C, 0xE7, 0xBC, 0x4F, 0x5A, 0x34}
+
 // wiring of each half
-#define MATRIX_ROW_PINS { C6, D7, E6, B4, B5 }
-#define MATRIX_COL_PINS { B6, B2, B3, B1, F7, F6, F5 }
-#define MATRIX_COL_PINS_RIGHT { F5, F6, F7, B1, B3, B2, B6 }
+#define MATRIX_ROW_PINS { F5, F6, F7, B1, B3, B2 }
+#define MATRIX_COL_PINS { F4, D3, D0, D4, C6, D7, E6 }
+#define MATRIX_COL_PINS_RIGHT { E6, D7, C6, D4, D0, D3, F4 }
 
 #define DIODE_DIRECTION ROW2COL
 
@@ -44,6 +47,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SOFT_SERIAL_PIN D1
 #define SPLIT_HAND_PIN D2
 
+#define ENCODERS_PAD_A { B4 }
+#define ENCODERS_PAD_B { B5 }
+#define ENCODERS_PAD_A_RIGHT { B4 }
+#define ENCODERS_PAD_B_RIGHT { B5 }
 
 /* define if matrix has ghost */
 //#define MATRIX_HAS_GHOST
@@ -55,3 +62,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
+
+/* ws2812 RGB LED */
+#define RGB_DI_PIN B6
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 20    // Number of LEDs
+#define RGBLED_SPLIT { 10, 10 }
